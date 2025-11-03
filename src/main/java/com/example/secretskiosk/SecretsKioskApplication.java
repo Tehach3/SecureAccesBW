4d57d7584e1d3504615931737cb3f3c7561426dc
Martin <tehach3@gmail.com>
2025-11-03 07:17:19 -0300

    initial commit

---
+++ src/main/java/com/example/secretskiosk/SecretsKioskApplication.java
@@ -0,0 +1,13 @@
+package com.example.secretskiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretsKioskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretsKioskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/secretskiosk/SecretsKioskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secretskiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretsKioskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

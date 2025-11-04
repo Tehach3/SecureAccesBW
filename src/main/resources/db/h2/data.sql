ed1b92805ae4c0d6be0d7df3ca3f8aede256c36e
Martin <tehach3@gmail.com>
2025-11-04 14:08:26 -0300

    fix h2 db

--- src/main/resources/db/h2/data.sql
+++ src/main/resources/db/h2/data.sql
@@ -1,5 +1,3 @@
-INSERT INTO base.roles
-(id, code, name, description, created_at, updated_at)
-VALUES
-(RANDOM_UUID(), 'admin', 'Admin', 'Administrator role with full permissions', NOW(), NOW()),
-(RANDOM_UUID(), 'user',  'User',  'Standard user for system',                NOW(), NOW());
+INSERT INTO base.roles (id, code, name, description, created_at, updated_at)
+VALUES (RANDOM_UUID(),'admin','Admin','Administrator role with full permissions', NOW(), NOW()),
+       (RANDOM_UUID(),'user','User','Standard user for system', NOW(), NOW());
